be3180704a9f34ad9cd8fb12b70c4a1392b41128
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2025-05-12 08:20:18 -0400

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WNTRDividendDashboard from './components/WNTRDividendDashboard';
+
+function App() {
+  return (
+    <div className="App p-4">
+      <WNTRDividendDashboard />
+    </div>
+  );
+}
+
+export default App;
